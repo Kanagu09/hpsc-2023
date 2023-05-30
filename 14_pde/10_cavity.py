60e941b13ba0ddafbcf1a7eba362e52a893ca747
Masaki Otsubo <kanagu.remix@gmail.com>
2023-05-31 00:15:26 +0900

    assignment c++

--- 14_pde/10_cavity.py
+++ 14_pde/10_cavity.py
@@ -1,6 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+debug_full = True
+
+def print_array(name, array):
+    print(name, end="")
+    print(": [", end="")
+    for value in array:
+        print("{:.6g}".format(value), end=",")
+    print("]")
+
+def print_array2(name, array):
+    print(name, end="")
+    print(": [")
+    for array1 in array:
+        print("[", end="")
+        for value in array1:
+            print("{:.6g}".format(value), end=",")
+        print("]")
+    print("]")
+
 nx = 41
 ny = 41
 nt = 500
@@ -11,13 +30,37 @@ dt = .01
 rho = 1
 nu = .02
 
+if debug_full:
+    print("nx:", nx)
+    print("ny:", ny)
+    print("nt:", nt)
+    print("nit:", nit)
+    print("dx:", dx)
+    print("dy:", dy)
+    print("dt:", dt)
+    print("rho:", rho)
+    print("nu:", nu)
+    print()
+
 x = np.linspace(0, 2, nx)
 y = np.linspace(0, 2, ny)
+
+if debug_full:
+    print_array("x", x)
+    print_array("y", y)
+    print()
 
 u = np.zeros((ny, nx))
 v = np.zeros((ny, nx))
 p = np.zeros((ny, nx))
 b = np.zeros((ny, nx))
+
+if debug_full:
+    print_array2("u", u)
+    print_array2("v", v)
+    print_array2("p", p)
+    print_array2("b", b)
+
 X, Y = np.meshgrid(x, y)
 
 for n in range(nt):
@@ -27,6 +70,10 @@ for n in range(nt):
                     ((u[j, i+1] - u[j, i-1]) / (2 * dx) + (v[j+1, i] - v[j-1, i]) / (2 * dy)) -\
                     ((u[j, i+1] - u[j, i-1]) / (2 * dx))**2 - 2 * ((u[j+1, i] - u[j-1, i]) / (2 * dy) *\
                      (v[j, i+1] - v[j, i-1]) / (2 * dx)) - ((v[j+1, i] - v[j-1, i]) / (2 * dy))**2)
+
+    if debug_full:
+        print_array2("b", b)
+
     for it in range(nit):
         pn = p.copy()
         for j in range(1, ny-1):
@@ -39,6 +86,10 @@ for n in range(nt):
         p[0, :] = p[1, :]
         p[:, 0] = p[:, 1]
         p[-1, :] = 0
+
+    if debug_full:
+        print_array2("p", p)
+
     un = u.copy()
     vn = v.copy()
     for j in range(1, ny-1):
@@ -61,9 +112,17 @@ for n in range(nt):
     v[-1, :] = 0
     v[:, 0]  = 0
     v[:, -1] = 0
-    plt.contourf(X, Y, p, alpha=0.5, cmap=plt.cm.coolwarm)
-    plt.quiver(X[::2, ::2], Y[::2, ::2], u[::2, ::2], v[::2, ::2])
-    plt.pause(.01)
-    plt.clf()
+    
+    if debug_full:
+        print_array2("u", u)
+        print_array2("v", v)
+
+    # plt.contourf(X, Y, p, alpha=0.5, cmap=plt.cm.coolwarm)
+    # plt.quiver(X[::2, ::2], Y[::2, ::2], u[::2, ::2], v[::2, ::2])
+    # plt.pause(.01)
+    # plt.clf()
+
+print_array2("u", u)
+print_array2("v", v)
 
 # 2次元 Navier-Stokes方程式
